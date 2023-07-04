4558d6da9aad3be540913aa9aa4fc737b0ab1272
edward.carmody <edward.carmody@gmail.com>
2023-07-04 12:36:47 -0400

    added env. variables for buckets

--- src/ProcessVideo/index.js
+++ src/ProcessVideo/index.js
@@ -1,22 +1,32 @@
 exports.handler = async (event) => {
   const AWS = require('aws-sdk');
-  const s3 = new AWS.S3();
-  // TODO implement
+
+  const s3IN = new AWS.S3();
+  const s3OUT = new AWS.S3();
+  // get env variables
+  const inBucket = process.env.VIDINBUCKET_BUCKET_NAME;
+  const outBucket = process.env.VIDOUTBUCKET_BUCKET_NAME;
+  
+  var params = {Bucket: inBucket, Key: JSON.parse(event.body).filename}
+  var s3file = s3IN.getObject(params)
+  
+    
+  // call linux cmds
   const execSync = require('child_process').execSync;
+  const output1 = execSync('/opt/ffmpeglib/ffprobe -h', { encoding: 'utf-8' });  
   const output = execSync('/opt/ffmpeglib/ffmpeg -h', { encoding: 'utf-8' });  
-  // the default is 'buffer'
-  //console.log('Output was:\n', output);
-  const requestBody = JSON.parse(event.body);
+
+
 
   // Parse the JSON data
-  //const jsonData = JSON.parse(requestBody);
-  //const name = jsonData.cmd;
-  //const filename = jsonData.filename;
-  //let concatenatedString = name + " " + filename;
+  const cmd = JSON.parse(event.body).cmd;
+  const filename = JSON.parse(event.body).filename;
+  //let concatenatedString = cmd + " " + filename + " " + output;
+  let concatenatedString = cmd + " " + filename ;
   
   const response = {
       statusCode: 200,
-      body: JSON.stringify(event.body),
+      body: JSON.stringify(concatenatedString),
   };
   return response;
 };
