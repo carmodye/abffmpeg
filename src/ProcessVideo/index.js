d3081b9ae6e9ebcbe3090c0e403ab0c458d6f2a3
edward.carmody <edward.carmody@gmail.com>
2023-07-04 14:42:55 -0400

    added util files from examples

--- src/ProcessVideo/index.js
+++ src/ProcessVideo/index.js
@@ -1,13 +1,20 @@
 exports.handler = async (event) => {
   const AWS = require('aws-sdk');
+  
+  const s3Util = require('./s3-util'),
+	childProcessPromise = require('./child-process-promise'),
+	path = require('path'),
+	os = require('os'),
+  OUTPUT_BUCKET = process.env.VIDOUTBUCKET_BUCKET_NAME,
+  INPUT_BUCKET = process.env.VIDINBUCKET_BUCKET_NAME;
 
   const s3IN = new AWS.S3();
   const s3OUT = new AWS.S3();
   // get env variables
-  const inBucket = process.env.VIDINBUCKET_BUCKET_NAME;
-  const outBucket = process.env.VIDOUTBUCKET_BUCKET_NAME;
+  //const inBucket = process.env.VIDINBUCKET_BUCKET_NAME;
+  //const outBucket = process.env.VIDOUTBUCKET_BUCKET_NAME;
   
-  var params = {Bucket: inBucket, Key: JSON.parse(event.body).filename}
+  var params = {Bucket: INPUT_BUCKET, Key: JSON.parse(event.body).filename}
   var s3file = s3IN.getObject(params)
   
     
